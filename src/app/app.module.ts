8cfa559d81b16c9a40487e6611de04e03f04c9ff
Glenn Pinto <gvpinto@gmail.com>
2021-10-30 22:12:29 -0400

    36. Assignment on data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     ServersComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
+    UserComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
