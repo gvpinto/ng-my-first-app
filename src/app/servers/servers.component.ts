5969c755b887354317d7c60a419c4c585ac2bc70
Glenn Pinto <gvpinto@gmail.com>
2021-10-30 20:02:29 -0400

    34. Two was Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,7 +14,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server was Created';
-  serverName: string = '';
+  serverName: string = 'Test Server';
 
   constructor() {
     setTimeout(() => {
